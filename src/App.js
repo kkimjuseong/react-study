e8cc2864a1bb1b3b93de9839e47e5745f0b7b63b
kkimjuseong <rofodzl1@naver.com>
2024-06-12 11:04:17 +0900

    # react props1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ const App = () => {
 
     return (
         <>
-            <ExpenseItem/>
+            <ExpenseItem title='치킨먹음' price={24000} date={new Date(2024,6,3)} />
+            <ExpenseItem title='족발먹음' price={40000} date={new Date(2024,6,7)} />
+            <ExpenseItem title='국밥먹음' price={10000} date={new Date(2024,6,12)} />
         </>
     );
 }
--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({date, title, price: exPrice}) => {
+
+    // console.log('props: ', aaa);
 
     // 변수 선언
-    const expenseDate = new Date(2024, 6, 12);
-    const expenseTitle = '점심식사';
-    const expensePrice = 8000;
+    // const expenseDate = date;
+    // const expenseTitle = title;
+    // const expensePrice = exPrice;
 
     // 함수 선언
 
@@ -17,21 +19,21 @@ const ExpenseItem = () => {
 
     const makeFormattedDate = () => {
 
-        const year = expenseDate.getFullYear();
-        const month = expenseDate.getMonth();
-        const date = expenseDate.getDate();
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const day = date.getDate();
 
-        return `${year}년 ${make2digit(month)}월 ${make2digit(date)}일`;
+        return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
     };
 
     // 원화 표기법으로 변환
-    const formattedPrice = new Intl.NumberFormat('ko-KR').format(expensePrice);
+    const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice);
 
     return (
         <div className='expense-item'>
             <div>{makeFormattedDate()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{formattedPrice}원</div>
             </div>
         </div>
