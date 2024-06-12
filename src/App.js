2dcc7f43ce8ffdf377b9e6ee6e437af42f95cfe6
kkimjuseong <rofodzl1@naver.com>
2024-06-12 09:58:00 +0900

    # jsx 문법 및 규칙

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // jsx 문법
+    // const $h2 = React.createElement('h2', null, '방가방가');
+    const $h2 = <h2>방가방가 우리친구 햄토리</h2>
+
+    // jsx 규칙
+    // 1. return 태그안에있는 태그는 반드시 하나의 태그로 묶여야함
+    // 2. 빈 태그(닫는 태그가 없는) 는 반드시 /> 로 마감해야함
+    // 3. 태그의 class 속성은 자바스크립트 키워드 class 와 겹쳐서 className 으로 표기
+    // 4. 의미없는 부모는 React.Fragment 로 묶으면 된다. -> 축약표현 <> 로 가능
+    // 5. 변수값이나 함수를 출력할 때는 {}로 감싸면 된다.
+
+
+    const hello = '안녕하세요';
+    return (
+        <>
+            <div className="App">
+                <h1>{hello}</h1>
+                {$h2}
+            </div>
+            <div className='noname'>
+                <input type="text" />
+                <label htmlFor=""></label>
+            </div>
+        </>
+);
 }
 
 export default App;
