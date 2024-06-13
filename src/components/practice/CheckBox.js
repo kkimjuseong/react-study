d44ee98e5e9a66e11940a3b4ed8588fa82b6b0be
kkimjuseong <rofodzl1@naver.com>
2024-06-13 11:24:49 +0900

    # checkBox

--- src/components/practice/CheckBox.js
+++ src/components/practice/CheckBox.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './CheckBoxStyle.css';
 
-const CheckBox = () => {
+function CheckBoxStyle() {
+
+    // 체크상태를 관리
+    const [isChecked, setIsChecked] = useState(false);
+
+    const checkChangeHandler = e => {
+        setIsChecked(!isChecked);
+    };
+
     return (
-        <>
-            <div className="checkbox-container">
-                <input
-                    type="checkbox"
-                    id="styled-checkbox"
-                />
-                <label
-                    htmlFor="styled-checkbox"
-
-                >
-                    Check me!
-                </label>
-            </div>
-        </>
+        <div className="checkbox-container">
+            <input
+                type="checkbox"
+                id="styled-checkbox"
+                checked={isChecked}
+                onChange={checkChangeHandler}
+            />
+            <label
+                htmlFor="styled-checkbox"
+                className={isChecked ? 'checked' : 'unchecked'}
+            >
+                Check me!
+            </label>
+        </div>
     );
-};
+}
 
-export default CheckBox;
+export default CheckBoxStyle;
