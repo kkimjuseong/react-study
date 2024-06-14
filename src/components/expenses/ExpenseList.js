78fcdf2ab7bde2e0f87e201ad19a51de43cc1c16
kkimjuseong <rofodzl1@naver.com>
2024-06-14 09:37:51 +0900

    # 지출항목 배열관리하기

--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -1,38 +1,45 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import ExpenseFilter from './ExpenseFilter';
+import './ExpenseList.css'
 
-const ExpenseList = ({ expenses }) => {
-
+const ExpenseList = ({expenses}) => {
     // 선택된 연도로 재 렌더링하기 위해 연도를 상태값으로 관리
-    const [filteredYear, setFilteredYear]
-        = useState(new Date().getFullYear());
+    const [filteredYear, setFilteredYear] = useState(
+        new Date().getFullYear().toString()
+    );
 
     const onFilterChange = (filteredYear) => {
-        // ExpenseFilter 에 있는 선택된 연도값을 여기서 출력!
-        console.log('ExpenseList: ', filteredYear);
         setFilteredYear(filteredYear);
     };
 
-    // App.js 에서 받은 expenses 배열을 <ExpenseItem> 배열로 변환하는 함수
-    // const convertToComponentArray = () => {
-    //   return expenses
-    //         .map(ex => <ExpenseItem title={ex.title} price={ex.price} date={ex.date} />);
-    // };
+    // 연도로 필터링한 배열
+    const filteredExpenses = expenses.filter(
+        (ex) => ex.date.getFullYear().toString() === filteredYear
+    );
+
+
+    // 지출데이터가 없을 때 보여줄 태그
+    // 기본 설정값을 지출항목이 없음으로 지정하고 밑에서 지출항목이 생겼을때 렌더링
+    let content = <p>지출 항목이 없습니다.</p>;
+
+    // 지출데이터가 있을 때 보여줄 태그
+    if (filteredExpenses.length > 0) {
+        content = filteredExpenses.map(({title, price, date}) => (
+            <ExpenseItem
+                key={Math.random().toString()}
+                title={title}
+                price={price}
+                date={date}
+            />
+        ));
+    }
+
 
     return (
         <div className="expenses">
-            <ExpenseFilter onChangeFilter={onFilterChange} />
-            {expenses
-                .filter(ex => ex.date.getFullYear().toString() === filteredYear.toString())
-                .map((ex) => (
-                <ExpenseItem
-                    key={Math.random().toString()}
-                    title={ex.title}
-                    price={ex.price}
-                    date={ex.date}
-                />
-            ))}
+            <ExpenseFilter onChangeFilter={onFilterChange}/>
+            {content}
         </div>
     );
 };
