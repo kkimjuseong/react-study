cc1e3fd93ef1cbdec8699d4746aebfd0f1da4f0a
juseong <rofodzl1@naver.com>
2024-06-21 09:50:15 +0900

    # Meals 관련 컴포넌트 생성[

--- src/components/Food/Meals/MealItem/MealItemForm.js
+++ src/components/Food/Meals/MealItem/MealItemForm.js
@@ -5,17 +5,17 @@ import styles from './MealItemForm.module.scss';
 const MealItemForm = (props) => {
     return (
         <form className={styles.form}>
-            {/*<Input*/}
-            {/*    label='수량'*/}
-            {/*    input={{*/}
-            {/*        id: 'amount_' + props.id,*/}
-            {/*        type: 'number',*/}
-            {/*        min: '1',*/}
-            {/*        max: '5',*/}
-            {/*        step: '1',*/}
-            {/*        defaultValue: '1',*/}
-            {/*    }}*/}
-            {/*/>*/}
+            <Input
+                label='수량'
+                // input={{
+                //   id: 'amount_' + props.id,
+                //   type: 'number',
+                //   min: '1',
+                //   max: '5',
+                //   step: '1',
+                //   defaultValue: '1',
+                // }}
+            />
             <button>담기</button>
         </form>
     );
