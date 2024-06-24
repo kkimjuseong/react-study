ac1b0784fbd2e80b899f7172f80c72ad4f64292a
juseong <rofodzl1@naver.com>
2024-06-24 09:24:58 +0900

    # 장바구니 아이템 렌더링

--- src/components/Food/Cart/Cart.js
+++ src/components/Food/Cart/Cart.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Cart.module.scss';
 import CartModal from './CartModal';
+import CartContext from '../../../store/Cart-context';
+import CartItem from './CartItem';
 
-const DUMMY_CART = [
-    {
-        id: 'c1',
-        name: '스시',
-        amount: 2,
-        price: 46000,
-    },
-    {
-        id: 'c2',
-        name: '띠드버거',
-        amount: 1,
-        price: 12000,
-    },
-];
+// const DUMMY_CART = [
+//   {
+//     id: 'c1',
+//     name: '스시',
+//     amount: 2,
+//     price: 46000,
+//   },
+//   {
+//     id: 'c2',
+//     name: '띠드버거',
+//     amount: 1,
+//     price: 12000,
+//   },
+// ];
 const Cart = ({ onClose }) => {
+    const { cartItems } = useContext(CartContext);
+
     const {
         'cart-items': cartItemStyle,
         total,
@@ -29,8 +33,11 @@ const Cart = ({ onClose }) => {
         <CartModal onClose={onClose}>
             {/* 주문 내역 */}
             <ul className={cartItemStyle}>
-                {DUMMY_CART.map((cartItem) => (
-                    <li key={cartItem.id}>{cartItem.name}</li>
+                {cartItems.map((cartItem) => (
+                    <CartItem
+                        key={cartItem.id}
+                        cart={cartItem}
+                    />
                 ))}
             </ul>
             <div className={total}>
@@ -38,7 +45,12 @@ const Cart = ({ onClose }) => {
                 <span>58,000원</span>
             </div>
             <div className={actions}>
-                <button className={btnAlt} onClick={onClose}>닫기</button>
+                <button
+                    className={btnAlt}
+                    onClick={onClose}
+                >
+                    닫기
+                </button>
                 <button className={button}>주문</button>
             </div>
         </CartModal>
