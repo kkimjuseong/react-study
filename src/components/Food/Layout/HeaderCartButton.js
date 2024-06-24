3c63ba1b771909498a04a17880131019763a3992
juseong <rofodzl1@naver.com>
2024-06-24 12:40:53 +0900

    # 장바구니 애니메이션 처리

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,11 +1,15 @@
 
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from './CartIcon';
 import styles from './HeaderCartButton.module.scss';
 import CartContext from "../../../store/Cart-context";
 
 const HeaderCartButton = ({ onShow }) => {
 
+    // bump 에니메이션을 제거하는 상태변수
+    const [isBump, setIsBump] = useState(false);
+
+    // 장바구니 배열
     const {cartItems} = useContext(CartContext);
 
     const numberOfCart = cartItems.reduce((acc, cur) => acc + cur.amount, 0);
@@ -19,10 +23,23 @@ const HeaderCartButton = ({ onShow }) => {
     // };
 
 
-    const {button, icon, badge} = styles;
+    const {button, icon, badge, bump} = styles;
+
+    useEffect(() => {
+        if(cartItems.length === 0) return
+        setIsBump(true);
+
+        // 애니메이션 실행 후 클래스를 제거
+        const timer = setTimeout(() => {
+            setIsBump(false);
+        }, 300)
+
+        return () => clearTimeout(timer);
+
+    }, [cartItems]);
 
     return (
-        <button className={button} onClick={onShow}>
+        <button className={`${button} ${isBump ? bump : undefined}`} onClick={onShow}>
       <span className={icon}>
         <CartIcon />
       </span>
