30355da9ac99be972eeb517b357e962feede51e2
kkimjuseong <rofodzl1@naver.com>
2024-07-03 10:06:54 +0900

    # 서버에서 정보 받아오기

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,39 +1,34 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
-const DUMMY_EVENTS = [
-    {
-        id: '1',
-        title: '여름맞이 이벤트',
-        image: 'https://m.puroluna.com/file_data/iffl8888/2019/06/18/e6a78ac2fbfa26acf240feb86380a47f.jpg',
-        date: '2024-07-20',
-        description: '여름맞이 이벤트입니다. 재밌겠지?'
-    },
-    {
-        id: '2',
-        title: '건강관리 이벤트',
-        image: 'https://www.nhis.or.kr/static/alim/paper/oldpaper/202109/assets/images/sub/event01_mo.jpg',
-        date: '2024-07-15',
-        description: '건강 이벤트입니다. 건강하겠지?'
-    },
-];
 
 const Events = () => {
+
+    const [eventList, setEventList] = useState([]);
+
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('http://localhost:8282/events');
+            const jsonData = await response.json();
+            setEventList(jsonData);
+        })();
+        ;
+    }, []);
+
     return (
         <>
             <h1>Events Page</h1>
             <ul>
-                {
-                    DUMMY_EVENTS.map(event => (
-                        <li key={event.id}>
-                            <Link to={event.id}>{event.title}</Link>
-                        </li>
-                    ))
-                }
+                {eventList.map((event) => (
+                    <li key={event.id}>
+                        <Link to={event.id}>{event.title}</Link>
+                    </li>
+                ))}
             </ul>
         </>
-    )
-}
+    );
+};
 
-export default Events
+export default Events;
