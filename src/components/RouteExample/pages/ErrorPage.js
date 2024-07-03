814868731825f92592b5228ae222641bf301bad2
kkimjuseong <rofodzl1@naver.com>
2024-07-03 12:02:30 +0900

    # useRouteError 사용

--- src/components/RouteExample/pages/ErrorPage.js
+++ src/components/RouteExample/pages/ErrorPage.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import MainNavigation from "../layout/MainNavigation";
+import MainNavigation from '../layout/MainNavigation';
+import { useRouteError } from 'react-router-dom';
+import EventsNavigation from '../layout/EventsNavigation';
 
 const ErrorPage = () => {
+
+    // 에러가 발생했을 때 throw에 전달한 객체를 읽는 방법
+    const error = useRouteError();
+
+    let errorMessage;
+
+    if (error.status === 400) {
+        errorMessage = JSON.parse(error.data).message;
+        // console.log(errorMessage);
+    }
+
+    if (error.status === 404) {
+        errorMessage = '페이지를 찾을 수 없습니다. URL을 확인하세요!';
+    }
+
     return (
         <>
-            <MainNavigation/>
+            <MainNavigation />
+            <EventsNavigation />
             <main>
-                <h1>페이지를 찾을 수 없습니다.</h1>
-                <p>요청하신 페이지의 주소를 다시 확인해 주세요.</p>
+                <h1>에러가 발생했습니다!</h1>
+                <p>{errorMessage}</p>
             </main>
         </>
     );
--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -8,7 +8,7 @@ const Events = () => {
 
     // loader 가 리턴한 데이터 받아오기
     const eventList = useLoaderData();
-    // console.log(aaa);
+    console.log(eventList);
 
     return (
         <>
@@ -23,10 +23,23 @@ export default Events;
 // loader 를 app.js 로부터 아웃소싱
 export const loader = async () => {
 
+
     // 이 페이지가 열릴때 자동으로 트리거가 되어 호출되는 함수
     // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있다.
 
     const response = await fetch('http://localhost:8282/events');
+
+    if (!response.ok) {
+        const errorText = await response.text();
+        throw new Response(
+            JSON.stringify({ message: errorText }),
+        {
+            status: response.status,
+        }
+        );
+    }
+    return response;    // ok 일 경우 events[]
+
     // const jsonData = await response.json();
 
     // loader 가 리턴한 데이터는 loader 를 선언한 컴포넌트와 그 하위 컴포넌트에서
@@ -34,5 +47,4 @@ export const loader = async () => {
     // console.log(jsonData)
 
     // loader 에서 fetch 의 결과를 바로 리턴하면 알아서 json 을 추출해준다.
-    return response;
 };
