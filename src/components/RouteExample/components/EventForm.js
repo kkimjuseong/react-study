edbe92aceccf0adc04acba70e468f7d32fc09339
kkimjuseong <rofodzl1@naver.com>
2024-07-04 11:39:41 +0900

    # EventForm 서버통신 세이브기능

--- src/components/RouteExample/components/EventForm.js
+++ src/components/RouteExample/components/EventForm.js
@@ -12,6 +12,27 @@ const EventForm = ({ method, event={} }) => {
         'start-date': date
     } = event;
 
+    // 날짜 형식을 변경 (yyyy-MM-dd)
+    /**
+     *
+     * @param date - yyyy년 MM월 dd일
+     */
+    const convertDateFormat = (date) => {
+        const [yearPart, monthDayPart] = date.split('년 ');
+        const [monthPart, dayPart] = monthDayPart.split('월 ');
+
+        const day = dayPart.replace('일', '');
+
+        // console.log('date: ', { yearPart, monthPart, day });
+
+        return `${yearPart}-${monthPart}-${day}`;
+    };
+
+    let formatDate;
+    if (event.date) {
+        formatDate = convertDateFormat(date);
+    }
+
     // const { eventId: id } = useParams();
     const navigate = useNavigate();
 
@@ -21,8 +42,40 @@ const EventForm = ({ method, event={} }) => {
         navigate('..');
     };
 
+    const submitHandler = e => {
+        e.preventDefault();
+        // console.log('form이 제출됨!');
+
+        // input에 입력한 값 가져오기
+        const formData = new FormData(e.target);
+        // console.log('form: ', formData.get('title'));
+
+        // 서버에 보낼 데이터
+        const payload = {
+            title: formData.get('title'),
+            desc: formData.get('description'),
+            imageUrl: formData.get('image'),
+            beginDate: formData.get('date')
+        };
+
+        // 서버로 페칭 즉시실행함수 사용
+        (async () => {
+            const response = await fetch(`http://localhost:8282/events`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            })
+
+            navigate('/events');
+
+        })();
+
+    };
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={submitHandler} noValidate>
             <p>
                 <label htmlFor="title">Title</label>
                 <input
@@ -50,7 +103,7 @@ const EventForm = ({ method, event={} }) => {
                     type="date"
                     name="date"
                     required
-                    defaultValue={event ? date : ''}
+                    defaultValue={event ? formatDate : ''}
                 />
             </p>
             <p>
