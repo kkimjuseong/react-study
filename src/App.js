72949da6185cbfbadd7e354eaef631271ec5c5ab
kkimjuseong <rofodzl1@naver.com>
2024-07-04 10:23:04 +0900

    # loader 로 상세조회하기

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Home from './components/RouteExample/pages/Home';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './components/RouteExample/layout/RootLayout';
 import ErrorPage from './components/RouteExample/pages/ErrorPage';
-import Events, {loader} from "./components/RouteExample/pages/Events";
-import EventDetail from "./components/RouteExample/pages/EventDetail";
+import Events, { loader as eventListLoader} from "./components/RouteExample/pages/Events";
+import EventDetail, { loader as eventDetailLoader} from "./components/RouteExample/pages/EventDetail";
 import EventLayout from "./components/RouteExample/layout/EventLayout";
 import NewEvent from "./components/RouteExample/pages/NewEvent";
 
@@ -27,9 +27,14 @@ const router = createBrowserRouter([
                       element: <Events/>,
                       // 이 페이지가 열릴때 자동으로 트리거가 되어 호출되는 함수
                       // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있다.
-                      loader: loader,
+                      loader: eventListLoader,
                   },
-                  {path: ':eventId', element: <EventDetail />},
+                  {
+                      path: ':eventId',
+                      element: <EventDetail />,
+                      loader: eventDetailLoader,
+                  },
+
                   {path : 'new', element: <NewEvent />}
               ]
             },
--- src/components/RouteExample/pages/EventDetail.js
+++ src/components/RouteExample/pages/EventDetail.js
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import React from 'react';
+import {useLoaderData} from 'react-router-dom';
 import EventItem from "../components/EventItem";
 
 const EventDetail = () => {
 
-    const { eventId: id } = useParams();
-    const [ev, setEv] = useState({});
+    const ev = useLoaderData();
 
-    useEffect(() => {
-
-        (async () => {
-            const response = await fetch(`http://localhost:8282/events/${id}`);
-            const json = await response.json();
-            console.log('json: ', json);
-            setEv(json);
-        })();
-
-    }, []);
-
-    return <EventItem event={ev} />
+    return <EventItem event={ev}/>
 };
 
 export default EventDetail;
+
+
+export const loader = async (params) => {
+
+    const { eventId: id } = params;
+
+// use 로 시작하는 함수인 리액트 훅은 컴포넌트 내부에서만 사용가능하다.
+//     const {eventId: id} = useParams();
+//     const [ev, setEv] = useState({});
+
+
+    const response = await fetch(`http://localhost:8282/events/${id}`);
+
+    return await response.json()
+}
